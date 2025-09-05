c5d049b08fe42479b7a3e92632e9be1e349d4065
bircanbalki <bircanbalki@gmail.com>
2025-09-06 00:15:51 +0300

    case-4

--- # pusula_case.py
+++ # pusula_case.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 # ======================
 # 1. DATA LOADING
 # ======================
-df= pd.read_excel("C:\Users\birca\Documents\GitHub\Talent_Academy_Case_DT_2025.xlsx")
+df= pd.read_excel(r"C:\Users\birca\Documents\GitHub\Talent_Academy_Case_DT_2025.xlsx")
 print ("Dataset Shape: ", df.shape)
 print ("\nColumns: ", df.columns.tolist())
 print ("\nInfo: ")
@@ -27,3 +27,14 @@ print ("\nFirst Rows:\n ", df.head())
 print("\nDescriptive Statistics:\n ", df.describe(include="all"))
 # Eksik Değer Kontrolü
 print("\nMissing Values:\n ", df.isnull().sum())
+#Histogramlar
+df.hist(bins=30, figsize=(15,10))
+plt.suptitle("Numerical Features Histogram" , fontsize=16)
+plt.show()
+#Boxplot (numeric outlier kontolü için)
+numeric_cols = df.select_dtypes(include=[np.number]).columns
+for col in numeric_cols:
+    plt.figure(figsize=(6,3))
+    sns.boxplot(x=df[col])
+    plt.title(f"Boxplot - {col}")
+    plt.show()
