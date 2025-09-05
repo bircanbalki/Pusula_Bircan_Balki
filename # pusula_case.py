565e68ec57da5fbac99a39f035de171735cd4f14
bircanbalki <bircanbalki@gmail.com>
2025-09-06 00:00:50 +0300

    case-1

---
+++ # pusula_case.py
@@ -0,0 +1,11 @@
+# pusula_case.py
+# Author: Bircan Balkı
+# mail: bircanbalki@gmail.com 
+
+import pandas as pd 
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
